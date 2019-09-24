c0b39b37f8e13e64e199f423af1034ae5524e1ae
de-souza <43355143+de-souza@users.noreply.github.com>
2019-09-24 17:24:48 +0200

    Initialize all variables in the beginning

--- C/ex6_sizeof/main.c
+++ C/ex6_sizeof/main.c
@@ -4,15 +4,17 @@
 
 void main()
 {
-    int a = 0;
-    int size = sizeof(a) * CHAR_BIT;
+    int a;
+    int *b;
+    unsigned long int c;
+    int size;
+    
+    size = sizeof(a) * CHAR_BIT;
     printf("The size of an int is %d bytes\n", size);
 
-    int *b = &a;
     size = sizeof(b) * CHAR_BIT;
     printf("The size of a pointer is %d bytes\n", size);
 
-    unsigned long int c = 0;
     size = sizeof(c) * CHAR_BIT;
     printf("The size of an unsigned long int is %d bytes\n", size);
 }
