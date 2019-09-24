a2024725a7f3538ff8311e716dc88e6015b1277d
de-souza <43355143+de-souza@users.noreply.github.com>
2019-09-24 13:06:47 +0200

    Add "Hello, World!" program

---
+++ C/ex1_hello_world/main.c
@@ -0,0 +1,7 @@
+/* Simple "Hello, World!" program. */
+#include <stdio.h>
+
+void main()
+{
+    printf("Hello World!\n");
+}
--- C/ex1_intro/main.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void main()
-{
-    printf("\nExemple de programme c\n");
-}
