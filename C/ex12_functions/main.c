db3ffaa2d964a2aa1ae26d43af40327d5fd02249
de-souza <43355143+de-souza@users.noreply.github.com>
2019-09-26 01:17:34 +0200

    Add safe_scanf function

--- C/ex12_functions/main.c
+++ C/ex12_functions/main.c
@@ -1,27 +1,6 @@
 /* Implement various functions. */
+#include <stdarg.h>
 #include <stdio.h>
-
-int query_int(char *name)
-{
-    int i;
-    printf("Please enter a number. %s = ", name);
-    while (!scanf("%d", &i)) {
-        printf("This is not a number. Please try again. %s = ", name);
-        int c;
-        while ((c = getchar()) != '\n' && c != EOF) {}  // flush stdin
-    }
-    return i;
-}
-
-char query_char(char *name)
-{
-    char character;
-    printf("Please enter a character. %s = ", name);
-    scanf(" %c", &character);
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF) {}
-    return character;
-}
 
 int max(int a, int b)
 {
@@ -45,22 +24,46 @@ char Char(int i)
     return (char) i;
 }
 
+int safe_scanf(char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    int r=vscanf(format, args);
+    va_end(args);
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {}  // flush stdin
+    return r;
+}
+
 int main()
 {
     int a, b, i;
     char c;
+    char s[128];
 
-    printf("### max(a, b), min(a, b) ###\n");
-    a = query_int("a");
-    b = query_int("b");
+    printf("--- max(a, b), min(a, b) ---\n");
+    printf("Please enter a number. a = ");
+    while (!safe_scanf("%d", &a))
+        printf("This is not a number. Please try again. a = ");
+    printf("Please enter a number. b = ");
+    while (!safe_scanf("%d", &b))
+        printf("This is not a number. Please try again. b = ");
     printf("max(%d, %d) = %d\n", a, b, max(a,b));
     printf("min(%d, %d) = %d\n", a, b, min(a,b));
 
-    printf("### ascii(c) ###\n");
-    c = query_char("c");
+    printf("--- ascii(c) ---\n");
+    printf("Please enter a character. c = ");
+    safe_scanf("%c", &c);
     printf("ascii('%c') = %d\n", c, ascii(c));
 
-    printf("### Char(i) ###\n");
-    i = query_int("i");
+    printf("--- Char(i) ---\n");
+    printf("Please enter a number. i = ");
+    while (!safe_scanf("%d", &i))
+        printf("This is not a number. Please try again. i = ");
     printf("Char(%d) = '%c'\n", i, Char(i));
+
+    printf("--- firstcapital(s) ---\n");
+    printf("Please enter a string. s = ");
+    scanf("%127[^\n]s", s);
+    printf("%s\n", s);
 }
