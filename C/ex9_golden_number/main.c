2258607b9a30a4172ea0839205575473a3e48bf2
de-souza <43355143+de-souza@users.noreply.github.com>
2019-09-25 21:06:43 +0200

    Remove unnecessary variables

--- C/ex9_golden_number/main.c
+++ C/ex9_golden_number/main.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-    int tries=0, number, guess, error;
+    int tries=0, number, guess;
     time_t t;
     srand((unsigned) time(&t));     // initialize the random number generator
     number = rand() % 100;
@@ -14,8 +14,7 @@ int main()
         printf("Please enter a number between 0 and 99. n = ");
         while (!scanf("%d", &guess)) {
             printf("This is not a number. Please try again. n = ");
-            int c;
-            while ((c = getchar()) != '\n' && c != EOF) {}     // flush stdin
+            while ((guess = getchar()) != '\n' && guess != EOF) {}
         }
         if (guess < number) {
             printf("The number is higher.\n");
