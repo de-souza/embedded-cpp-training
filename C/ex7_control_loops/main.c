70da96173506fea05b991bbe5bab20ce40022f04
de-souza <43355143+de-souza@users.noreply.github.com>
2019-09-25 10:30:46 +0200

    Improve phrasing

--- C/ex7_control_loops/main.c
+++ C/ex7_control_loops/main.c
@@ -1,9 +1,9 @@
-/* Use a control loop to display characters a to z and their ASCII code. */
+/* Use a control loop to display characters a-z and their ASCII code. */
 #include <stdio.h>
 
 void main()
 {
-    printf("Characters a to z and their ASCII code:\n");
+    printf("Characters a-z and their ASCII code:\n");
     for (char c='a'; c <= 'z'; c++)
         printf("%c: %d\n", c, c);
 }
