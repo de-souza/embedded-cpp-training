c3d57ff8b5c537a5b0023589786c7e6365e83a00
de-souza <43355143+de-souza@users.noreply.github.com>
2019-09-25 14:17:54 +0200

    Add "golden number" program

---
+++ C/ex9_golden_number/main.c
@@ -0,0 +1,26 @@
+/* Ask the user to guess a number between 0 and 100. */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void main()
+{
+    int tries=0, number, guess, error;
+    time_t t;
+    srand((unsigned) time(&t));     // initialize the random number generator
+    number = rand() % 100;
+    while (guess != number || tries == 0) {
+        tries++;
+        printf("Please enter a number between 0 and 99. n = ");
+        while (!scanf("%d", &guess)) {
+            printf("This is not a number. Please try again. n = ");
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF) {}     // flush stdin
+        }
+        if (guess < number)
+            printf("The number is higher.\n");
+        else if (guess > number)
+            printf("The number is lower.\n");
+    }
+    printf("Well done. Total tries: %d.\n", tries);
+}
