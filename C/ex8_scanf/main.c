5277ebe2286079eae1fa1c75a00115d30d5af1fd
de-souza <43355143+de-souza@users.noreply.github.com>
2019-09-25 11:28:14 +0200

    Add program about the function scanf

---
+++ C/ex8_scanf/main.c
@@ -0,0 +1,20 @@
+/* Use the function scanf to read user input. */
+#include <stdio.h>
+
+void main()
+{
+    int i;
+    char c;
+    char *s;
+
+    printf("Please enter an integer. i = ");
+    scanf("%d", &i);
+
+    printf("Please enter a character. c = ");
+    scanf(" %c", &c);  // the leading whitespace is necessary with char
+
+    printf("Please enter a string. s = ");
+    scanf("%s", s);
+
+    printf("i = %d, c = %c, s = %s\n", i, c, s);
+}
