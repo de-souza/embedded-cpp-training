491923afa451e29b640c587fdf18fae66c7cf278
de-souza <43355143+de-souza@users.noreply.github.com>
2019-09-26 10:53:48 +0200

    Add function capitalize

--- C/ex12_functions/main.c
+++ C/ex12_functions/main.c
@@ -1,6 +1,7 @@
 /* Implement various functions. */
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 
 int max(const int a, const int b)
 {
@@ -22,6 +23,16 @@ char Char(const int i)
     if (i < 0 || i > 255)
         printf("The ASCII code must be between 0 and 255!\n");
     return (char) i;
+}
+
+char capital(char c)
+{
+    return ('a' <= c || c <= 'z') ? c - 32 : c;
+}
+
+void capitalize(char *restrict s)
+{
+    s[0] = capital(s[0]);
 }
 
 int safe_scanf(const char *restrict format, ...)
@@ -52,22 +63,23 @@ void main()
     printf("max(%d, %d) = %d\n", a, b, max(a,b));
     printf("min(%d, %d) = %d\n", a, b, min(a,b));
 
-    printf("--- ascii(c) ---\n");
+    printf("\n--- ascii(c) ---\n");
 
     printf("Please enter a character. c = ");
     safe_scanf("%c", &c);
     printf("ascii('%c') = %d\n", c, ascii(c));
 
-    printf("--- Char(i) ---\n");
+    printf("\n--- Char(i) ---\n");
 
     printf("Please enter a number. i = ");
     while (!safe_scanf("%d", &i))
         printf("This is not a number. Please try again. i = ");
     printf("Char(%d) = '%c'\n", i, Char(i));
 
-    printf("--- firstcapital(s) ---\n");
+    printf("\n--- capitalize(s) ---\n");
 
     printf("Please enter a string. s = ");
-    scanf("%127[^\n]s", s);
-    printf("%s\n", s);
+    safe_scanf("%127[^\n]", s);
+    capitalize(s);
+    printf("firstcapital(s) => s = %s\n", s);
 }
