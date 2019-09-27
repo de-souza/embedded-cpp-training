5b4353626b85f3ea95fe7bac1e22ab7ad8ae74a3
de-souza <43355143+de-souza@users.noreply.github.com>
2019-09-27 16:51:08 +0200

    Add program about stack and heap memory

---
+++ C/ex15_stack_heap/main.c
@@ -0,0 +1,56 @@
+/* Check which addresses are on the stack (static) and which are on the heap. */
+#include <stdio.h>
+#include <stdlib.h>
+
+static int static_var1;
+static int static_var2;
+static int static_var3;
+int global_var1;
+int global_var2;
+int global_var3;
+int global_arr1[8];
+int global_arr2[8];
+int global_arr3[8];
+
+void function1()
+{}
+
+void function2()
+{}
+
+void function3()
+{}
+
+void main()
+{
+    const int const1;
+    const int const2;
+    const int const3;
+    int local_var1;
+    int local_var2;
+    int local_var3;
+    int local_arr1[8];
+    int local_arr2[8];
+    int local_arr3[8];
+    printf("Address of local_var1: %p\n", &local_var1);
+    printf("Address of local_var2: %p\n", &local_var2);
+    printf("Address of local_var3: %p\n", &local_var3);
+    printf("Address of global_var1: %p\n", &global_var1);
+    printf("Address of global_var2: %p\n", &global_var2);
+    printf("Address of global_var3: %p\n", &global_var3);
+    printf("Address of static_var1: %p\n", &static_var1);
+    printf("Address of static_var2: %p\n", &static_var2);
+    printf("Address of static_var3: %p\n", &static_var3);
+    printf("Address of local_arr1: %p\n", &local_arr1);
+    printf("Address of local_arr2: %p\n", &local_arr2);
+    printf("Address of local_arr3: %p\n", &local_arr3);
+    printf("Address of global_arr1: %p\n", &global_arr1);
+    printf("Address of global_arr2: %p\n", &global_arr2);
+    printf("Address of global_arr3: %p\n", &global_arr3);
+    printf("Address of const1: %p\n", &const1);
+    printf("Address of const2: %p\n", &const2);
+    printf("Address of const3: %p\n", &const3);
+    printf("Address of function1: %p\n", &function1);
+    printf("Address of function2: %p\n", &function2);
+    printf("Address of function3: %p\n", &function3);
+}
