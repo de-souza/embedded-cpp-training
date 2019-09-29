88a060c03c6ff0186c6dd48813400ae36835c2fb
de-souza <43355143+de-souza@users.noreply.github.com>
2019-09-29 12:03:57 +0200

    Drop parentheses (sizeof is not a function)

--- C/ex06_sizeof/main.c
+++ C/ex06_sizeof/main.c
@@ -9,12 +9,12 @@ void main()
     unsigned long int c;
     int size;
     
-    size = sizeof(a) * CHAR_BIT;
+    size = sizeof a * CHAR_BIT;
     printf("The size of an int is %d bytes\n", size);
 
-    size = sizeof(b) * CHAR_BIT;
+    size = sizeof b * CHAR_BIT;
     printf("The size of a pointer is %d bytes\n", size);
 
-    size = sizeof(c) * CHAR_BIT;
+    size = sizeof c * CHAR_BIT;
     printf("The size of an unsigned long int is %d bytes\n", size);
 }
