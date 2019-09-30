b7f1aa40d32554c5117fbd648ae7cb397c9a6795
de-souza <43355143+de-souza@users.noreply.github.com>
2019-09-30 14:50:13 +0200

    Avoid repetition of \n

--- C/ex20_typedef_enum/main.c
+++ C/ex20_typedef_enum/main.c
@@ -14,18 +14,18 @@ typedef enum {
 const char *day_from_index(const int i)
 {
     switch (i) {
-    case 0: return "Monday\n";
-    case 1: return "Tuesday\n";
-    case 2: return "Wednesday\n";
-    case 3: return "Thursday\n";
-    case 4: return "Friday\n";
-    case 5: return "Saturday\n";
-    case 6: return "Sunday\n";
+    case 0: return "Monday";
+    case 1: return "Tuesday";
+    case 2: return "Wednesday";
+    case 3: return "Thursday";
+    case 4: return "Friday";
+    case 5: return "Saturday";
+    case 6: return "Sunday";
     }
 }
 
 int main()
 {
     for (day_t i=monday; i<=sunday; i++)
-        printf("%s", day_from_index(i));
+        printf("%s\n", day_from_index(i));
 }
