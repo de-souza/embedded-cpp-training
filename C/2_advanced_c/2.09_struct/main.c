29a49b2f55cfa7d352ee15d4ca080f3571c74dbc
de-souza <43355143+de-souza@users.noreply.github.com>
2019-09-30 17:33:33 +0200

    Improve clarity

--- C/2_advanced_c/2.09_struct/main.c
+++ C/2_advanced_c/2.09_struct/main.c
@@ -7,16 +7,16 @@ typedef struct {
     short year;
 } book_t;
 
-void list_titles(book_t shelf[], int max_i)
+void list_titles(book_t *shelf, int n)
 {
     book_t *ptr = shelf;
-    while (ptr != shelf + max_i + 1) {
+    while (ptr != shelf + n) {
         printf("%s\n", ptr->title);
         ptr++;
     }
 }
 
-void set_book(book_t shelf[], int i, book_t book)
+void set_book(book_t *shelf, int i, book_t book)
 {
     shelf[i] = book;
 }
@@ -30,9 +30,9 @@ int main()
         { "Daniel Kahneman", "Thinking, Fast and Slow", 2012 },
         { "Ivan Cukic", "Functional Programming in C++", 2018 },
     };
-    list_titles(shelf, 4);
-    puts("-- add book --");
-    book_t new_book = { "Peppa Pig: Up and Down", "Peppa Pig", 2019 };
-    set_book(shelf, 5, new_book);
     list_titles(shelf, 5);
+    puts("-- add book --");
+    book_t new_book = { "Peppa Pig", "Peppa Pig: Up and Down", 2019 };
+    set_book(shelf, 5, new_book);
+    list_titles(shelf, 6);
 }
