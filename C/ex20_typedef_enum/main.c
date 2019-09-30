412e39972d7723d2d9b818c94c8ade21eaeb7948
de-souza <43355143+de-souza@users.noreply.github.com>
2019-09-30 15:00:54 +0200

    Use enum everywhere

--- C/ex20_typedef_enum/main.c
+++ C/ex20_typedef_enum/main.c
@@ -1,31 +1,23 @@
 /* How to use typedef and enumerations. */
 #include <stdio.h>
 
-typedef enum {
-    monday,
-    tuesday,
-    wednesday,
-    thursday,
-    friday,
-    saturday,
-    sunday,
-} day_t;
+typedef enum {mon, tue, wed, thu, fri, sat, sun} day_t;
 
-const char *day_from_index(const int i)
+const char *string_from_day_t(day_t d)
 {
-    switch (i) {
-    case 0: return "Monday";
-    case 1: return "Tuesday";
-    case 2: return "Wednesday";
-    case 3: return "Thursday";
-    case 4: return "Friday";
-    case 5: return "Saturday";
-    case 6: return "Sunday";
+    switch (d) {
+    case mon: return "Monday";
+    case tue: return "Tuesday";
+    case wed: return "Wednesday";
+    case thu: return "Thursday";
+    case fri: return "Friday";
+    case sat: return "Saturday";
+    case sun: return "Sunday";
     }
 }
 
 int main()
 {
-    for (day_t i=monday; i<=sunday; i++)
-        printf("%s\n", day_from_index(i));
+    for (day_t d=mon; d<=sun; d++)
+        printf("%s\n", string_from_day_t(d));
 }
