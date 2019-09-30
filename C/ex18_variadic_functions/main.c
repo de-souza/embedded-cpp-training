d72af773ef206de5b8f09211b98382285effac7b
de-souza <43355143+de-souza@users.noreply.github.com>
2019-09-30 13:59:53 +0200

    Add program about variadic functions

---
+++ C/ex18_variadic_functions/main.c
@@ -0,0 +1,23 @@
+/* How to use variadic functions. */
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+void printf_newline(const char *restrict format, ...)
+{
+    char format_newline[strlen(format+2)];
+    strcpy(format_newline, format);
+    strcat(format_newline, "\n");
+    va_list args;
+    va_start (args, format);
+    vprintf (format_newline, args);
+    va_end (args);
+}
+
+int main()
+{
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    printf_newline("a = %d, b = %d, c = %d", a, b, c);
+}
