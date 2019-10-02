32c7717ad72249065aff6d77771fb5dadbe4da70
de-souza <43355143+de-souza@users.noreply.github.com>
2019-10-02 10:20:26 +0200

    Add program about union and bit fields

---
+++ C/2_advanced_c/2.11_union_bit_fields/main.c
@@ -0,0 +1,21 @@
+/* How to create bit fields and associate different types using union. */
+#include <stdio.h>
+
+int main()
+{
+    struct bit_fields {
+        unsigned first_bit: 1;
+        unsigned last_seven_bits : 7;
+    };
+    union my_union {
+        struct bit_fields as_bits;
+        char as_char;
+    } value;
+    value.as_char = 'b';
+    printf("value.as_char = '%c'\n", value.as_char);
+    printf("value.as_bits.first_bit = %d\n", value.as_bits.first_bit);
+    puts("-- flip first bit --");
+    value.as_bits.first_bit = 1;
+    printf("value.as_bits.first_bit = %d\n", value.as_bits.first_bit);
+    printf("value.as_char = '%c'\n", value.as_char);
+}
