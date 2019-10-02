0f1e5592b968ba951c0e22995b3fa17f183434ca
de-souza <43355143+de-souza@users.noreply.github.com>
2019-10-02 22:45:29 +0200

    Simplify code

--- C/2_advanced_c/2.12_linked_lists/main.c
+++ C/2_advanced_c/2.12_linked_lists/main.c
@@ -3,9 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define INDEX_MAX_SIZE 10   // 10 = max 8 digits + \n + \0
-#define YEAR_MAX_SIZE 6     // 6 = max 4 digits + \n + \0
-
 typedef struct book {
     char author[40];
     char title[30];
@@ -41,105 +38,133 @@ char safe_getchar()
 
 int enter_num(char *buffer, const int max_size)
 {
-    char *endptr;
     safe_fgets(buffer, max_size);
-    int n = strtol(buffer, &endptr, 10);
+    char *endptr;
+    int num = strtol(buffer, &endptr, 10);
     while (strlen(endptr) != 0 || endptr == buffer) {
         printf("Not a number. Please try again: ");
         safe_fgets(buffer, sizeof(buffer));
-        n = strtol(buffer, &endptr, 10);
+        num = strtol(buffer, &endptr, 10);
     }
-    return n;
+    return num;
 }
 
-int enter_index(const int max_idx)
+int index_of_last_book(book_t *head)
 {
-    char buffer[INDEX_MAX_SIZE];
+    book_t *current = head;
+    int idx = 1;
+    while ((current = current->next) != NULL)
+        idx++;
+    return idx;
+}
+
+int enter_index(book_t *head)
+{
+    char buffer[10];
     printf("Please enter an index: ");
-    int i = enter_num(buffer, sizeof(buffer));
-    while (i < 1 || i > max_idx) {
-        printf("Invalid index (1 <= i <= %d). Please try again: ", max_idx);
-        i = enter_num(buffer, sizeof(buffer));
-    }
-    return i;
+    int max_idx=index_of_last_book(head), idx;
+    while ((idx = enter_num(buffer, sizeof(buffer))) < 1 || idx > max_idx)
+        printf("Index out of range (0 < i < %d). Please try again: ", max_idx+1);
+    puts("");
+    return idx;
 }
 
-book_t *enter_book()
+book_t enter_book()
 {
-    book_t *book = malloc(sizeof(book_t));
-    char buffer[YEAR_MAX_SIZE];
-    puts("\nPlease enter the new book's characteristics.\n");
+    book_t book;
     printf("Please enter an author: ");
-    safe_fgets(book->author, sizeof(book->author));
+    safe_fgets(book.author, sizeof(book.author));
     printf("Please enter an title: ");
-    safe_fgets(book->title, sizeof(book->title));
+    safe_fgets(book.title, sizeof(book.title));
     printf("Please enter a year: ");
-    book->year = (short) enter_num(buffer, sizeof(buffer));
+    char buffer[6];
+    book.year = enter_num(buffer, sizeof(buffer));
     puts("");
     return book;
 }
 
-book_t *create_book(const char *restrict author, const char *restrict title, const short year)
+book_t generate_book(const char *restrict author, const char *restrict title, const short year)
 {
-    book_t *book = malloc(sizeof(book_t));
-    strcpy(book->author, author);
-    strcpy(book->title, title);
-    book->year = year;
+    book_t book;
+    strcpy(book.author, author);
+    strcpy(book.title, title);
+    book.year = year;
     return book;
 }
 
-book_t *add_book(book_t *head, book_t *book)
+void print_book(int idx, book_t *book)
 {
-    if (head == NULL)
-        return book;
-    book_t *ptr = head;
-    while (ptr->next != NULL)
-        ptr = ptr->next;
-    ptr->next = book;
-    return head;
+    printf("%d. %s - %s (%d)\n", idx, book->author, book->title, book->year);
 }
 
-book_t *remove_book(book_t *head)
+book_t *current_at_index(book_t *head, const int idx)
 {
-    if (head == NULL) {
-        puts("\nSorry, there is no book to delete. Please add a book first.\n");
-        return head;
-    }
-    puts("\nPlease choose the index of the book to delete.\n");
+    book_t *current = head;
+    for (int curr_idx=1; curr_idx < idx; curr_idx++)
+        current = current->next;
+    return current;
+}
+
+book_t *push(book_t *head, book_t book)
+{
+    book_t *temp = malloc(sizeof(*temp));
+    strcpy(temp->author, book.author);
+    strcpy(temp->title, book.title);
+    temp->year = book.year;
+    temp->next = head;
+    return temp;
+}
+
+book_t *pop(book_t *head)
+{
     book_t *temp = head;
-    int max_idx = 1;
-    while (temp->next != NULL) {
-        temp = temp->next;
-        max_idx++;
-    }
-    int idx = enter_index(max_idx);
-    if (idx == 1) {
-        temp = head->next;
-        free(head);
-        puts("");
-        return temp;
-    }
-    temp = head;
-    for (int i=1; i < idx-1; i++)
-        temp = temp->next;
-    book_t *deleted = temp->next;
-    temp->next = deleted->next;
-    free(deleted);
-    puts("");
+    head = head->next;
+    free(temp);
     return head;
 }
 
-void list_books(book_t *head)
+book_t *add(book_t *head)
+{
+    puts("\nPlease enter the new book's characteristics.\n");
+    book_t book = enter_book();
+    if (head != NULL) {
+        puts("Now, please enter the index of the book in the shelf.\n");
+        int idx = enter_index(head);
+        if (idx != 1) {
+            book_t *current = current_at_index(head, idx-1);
+            current->next = push(current->next, book);
+            return head;
+        }
+    }
+    return push(head, book);
+}
+
+book_t *delete(book_t *head)
+{
+    if (head != NULL) {
+        puts("\nPlease enter the index of the book to delete.\n");
+        int idx = enter_index(head);
+        if (idx != 1) {
+            book_t *current = current_at_index(head, idx-1);
+            current->next = pop(current->next);
+            return head;
+        }
+        return pop(head);
+    }
+    puts("\nSorry, there is no book to delete. Please add a book first.\n");
+    return head;
+}
+
+void list(book_t *head)
 {
     if (head != NULL) {
         puts("\nBooks currently on the shelf:\n");
-        book_t *ptr = head;
-        int i = 1;
-        while (ptr != NULL) {
-            printf("%d. %s - %s (%d)\n", i, ptr->author, ptr->title, ptr->year);
-            ptr = ptr->next;
-            i++;
-        }
+        book_t *current = head;
+        int idx = 1;
+        do {
+            print_book(idx++, current);
+            current = current->next;
+        } while (current != NULL);
         puts("");
     } else {
         puts("\nSorry, there is no book to list. Please add a book first.\n");
@@ -149,11 +174,11 @@ void list_books(book_t *head)
 int main()
 {
     book_t *head = NULL;
-    head = add_book(head, create_book("Brian Kernighan, Dennis Ritchie", "The C Programming Language", 1978));
-    head = add_book(head, create_book("Nassim Nicholas Taleb", "The Black Swan", 2008));
-    head = add_book(head, create_book("Nick Bostrom", "Superintelligence", 2008));
-    head = add_book(head, create_book("Daniel Kahneman", "Thinking, Fast and Slow", 2012));
-    head = add_book(head, create_book("Ivan Cukic", "Functional Programming in C++", 2018));
+    head = push(head, generate_book("Ivan Cukic", "Functional Programming in C++", 2018));
+    head = push(head, generate_book("Daniel Kahneman", "Thinking, Fast and Slow", 2012));
+    head = push(head, generate_book("Nick Bostrom", "Superintelligence", 2008));
+    head = push(head, generate_book("Nassim Nicholas Taleb", "The Black Swan", 2008));
+    head = push(head, generate_book("Brian Kernighan, Dennis Ritchie", "The C Programming Language", 1978));
     while (1) {
         puts("Main menu. Choose between:");
         puts("  1. Add a book.");
@@ -162,9 +187,9 @@ int main()
         puts("  Q. Quit program.");
         printf("Please enter your selection: ");
         switch (safe_getchar()) {
-        case '1': head = add_book(head, enter_book()); break;
-        case '2': head = remove_book(head); break;
-        case '3': list_books(head); break;
+        case '1': head = add(head); break;
+        case '2': head = delete(head); break;
+        case '3': list(head); break;
         case 'q':
         case 'Q': return 0;
         default: puts("\nPlease type either 1, 2, 3, or Q.\n"); break;
