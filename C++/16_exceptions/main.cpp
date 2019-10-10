be779db90e8bc379946d2111e1b0155633aff2db
de-souza <43355143+de-souza@users.noreply.github.com>
2019-10-10 17:20:22 +0200

    Flush std::cout before displaying error message

--- C++/16_exceptions/main.cpp
+++ C++/16_exceptions/main.cpp
@@ -6,11 +6,12 @@ int main()
     try {
         std::cout << "A valid digit: " << Digit(5) << std::endl;
     } catch (const std::exception& e) {
-        std::cout << e.what() << std::endl;
+        std::cout << std::endl << e.what() << std::endl;
     }
+
     try {
         std::cout << "An invalid digit: " << Digit(10) << std::endl;
     } catch (const std::exception& e) {
-        std::cout << e.what() << std::endl;
+        std::cout << std::endl << e.what() << std::endl;
     }
 }
