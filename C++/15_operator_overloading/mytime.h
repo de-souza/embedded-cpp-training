863d61b87f38adfa78413ae9220df756764cd989
de-souza <43355143+de-souza@users.noreply.github.com>
2019-10-10 14:27:56 +0200

    Revert 38905880623c93cb1f8a5ed8a5e7aab6e9c62398

--- C++/15_operator_overloading/mytime.h
+++ C++/15_operator_overloading/mytime.h
@@ -12,8 +12,9 @@ private:
     int mHours;
     int mMinutes;
     int mSeconds;
-    friend MyTime operator+(MyTime lhs, const MyTime& rhs);
     friend std::ostream& operator<<(std::ostream& os, const MyTime time);
 };
+
+MyTime operator+(MyTime lhs, const MyTime& rhs);
 
 #endif // MYTIME_H
