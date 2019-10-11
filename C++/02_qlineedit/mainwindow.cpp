4498f90f7baba043412411da461c1d34d2992b90
de-souza <43355143+de-souza@users.noreply.github.com>
2019-10-11 14:44:08 +0200

    Add pushButton and action on click

--- C++/02_qlineedit/mainwindow.cpp
+++ C++/02_qlineedit/mainwindow.cpp
@@ -13,3 +13,14 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    QPalette* palette = new QPalette();
+    palette->setColor(QPalette::Base, Qt::lightGray);
+    palette->setColor(QPalette::Text, Qt::gray);
+    ui->lineEdit_FirstName->setPalette(*palette);
+    delete palette;
+//    ui->lineEdit_FirstName->setDisabled(true);
+    ui->lineEdit_FirstName->setText("Grayed Out");
+}
