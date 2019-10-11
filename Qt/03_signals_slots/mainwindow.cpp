c9c2b2c687ff9999da47f8e169cce3ee52d49977
de-souza <43355143+de-souza@users.noreply.github.com>
2019-10-11 17:10:42 +0200

    Add program about signals and slots

---
+++ Qt/03_signals_slots/mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+//    connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(Quit()));
+    connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::Quit);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::Quit()
+{
+    close();
+}
+
