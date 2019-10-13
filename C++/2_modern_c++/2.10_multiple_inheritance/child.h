59e389d83049108621455cfcb311eeb6b03483f4
de-souza <43355143+de-souza@users.noreply.github.com>
2019-10-13 20:01:00 +0200

    Write base classes on the same line

--- C++/2_modern_c++/2.10_multiple_inheritance/child.h
+++ C++/2_modern_c++/2.10_multiple_inheritance/child.h
@@ -4,9 +4,7 @@
 #include "firstparent.h"
 #include "secondparent.h"
 
-class Child
-    : public FirstParent
-    , public SecondParent
+class Child: public FirstParent, public SecondParent
 {};
 
 #endif // CHILD_H
