eca5d102cb2bf1619b0889b830a77a884e64ab21
de-souza <43355143+de-souza@users.noreply.github.com>
2019-10-14 13:06:07 +0200

    Add program about QDate

---
+++ Qt/06_qdate/mainwindow.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->spinBox->setRange(-100, 100);
+    connect(ui->spinBox, QOverload<int>::of(&QSpinBox::valueChanged), this, &MainWindow::mShowNewDate);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::mShowNewDate()
+{
+    QDate new_date = ui->dateEdit->date();
+    new_date = new_date.addDays(ui->spinBox->value());
+    ui->label_result->setText(new_date.toString("dd/MM/yyyy"));
+}
