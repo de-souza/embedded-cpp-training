cf6a08bc70f9ae72415c1f574ed4d8b761b1a9cd
de-souza <43355143+de-souza@users.noreply.github.com>
2019-10-15 12:33:35 +0200

    Add program about QFile

---
+++ Qt/09_qfile/mainwindow.cpp
@@ -0,0 +1,33 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "about.h"
+
+#include <QFileDialog>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->actionExit, &QAction::triggered, this, &MainWindow::close);
+    connect(ui->actionAbout, &QAction::triggered, this, &MainWindow::mAbout);
+    connect(ui->actionOpen, &QAction::triggered, this, &MainWindow::mOpen);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::mAbout()
+{
+    About about(this);
+    about.exec();
+}
+
+void MainWindow::mOpen()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,
+                                                    QString("Open File"),
+                                                    QDir::homePath());
+}
