3a7b9f530ca328ed145ebaade0a83cbc370c2134
de-souza <43355143+de-souza@users.noreply.github.com>
2019-10-15 10:12:10 +0200

    Add about window

--- Qt/08_qdialog/mainwindow.cpp
+++ Qt/08_qdialog/mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "about.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->actionExit, &QAction::triggered, this, &MainWindow::close);
+    connect(ui->actionAbout, &QAction::triggered, this, &MainWindow::mAbout);
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +16,8 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::mAbout()
+{
+    About about(this);
+    about.exec();
+}
--- Qt/08_qdialog/mainwindow.h
+++ Qt/08_qdialog/mainwindow.h
@@ -17,5 +17,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    void mAbout();
 };
 #endif // MAINWINDOW_H
