f391df8122f45b7e82765c2fa76410f103adadc3
de-souza <43355143+de-souza@users.noreply.github.com>
2019-10-17 11:08:56 +0200

    Use QDirIterator::filePath instead of a variable

--- Qt/12_qregexp/main.cpp
+++ Qt/12_qregexp/main.cpp
@@ -5,9 +5,7 @@ int main()
 {
     QRegExp rx(".*\\.\\w{3}$");  // Match all files with an alphanumeric 3 char-long extension
     QDirIterator it(QDir::homePath(), QDir::Files, QDirIterator::Subdirectories);
-    while (it.hasNext()) {
-        QString filename = it.next();
-        if (rx.exactMatch(filename))
-            qDebug() << filename;
-    }
+    while (it.hasNext())
+        if (rx.exactMatch(it.next()))
+            qDebug() << it.filePath();
 }
