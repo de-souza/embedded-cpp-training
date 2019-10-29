2975cb7741ae8ad2474fd8511a3051b783cbba0a
de-souza <43355143+de-souza@users.noreply.github.com>
2019-10-29 17:58:01 +0100

    Output reachable IPs to text file

--- Unix/reachable_ips.sh
+++ Unix/reachable_ips.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 # Print the IP adresses that are reachable on the same network
-timeout=5
+filename="iplist.txt"
+timeout=3
 
 [[ $(ip --brief addr) =~ UP\ *([^\/]*) ]] && ip=${BASH_REMATCH[1]} || exit 1
 
 echo "This computer's IP address: $ip"
-echo "Other IPs on the network:"
 
+rm -f "$filename"
 for i in $(seq -f "${ip%\.*}.%g" 1 254); do
-    (ping "$i" -c 1 -w "$timeout" >/dev/null && echo "$i" &)
+    (ping "$i" -c 1 -w "$timeout" >/dev/null && echo "$i" >> "$filename" &)
 done
 sleep "$timeout"
+
+echo "IPs reachable on the network:"
+cat "$filename"
