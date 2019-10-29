c795063a43fdc72a8f9ef9425a69a3231512b9fd
de-souza <43355143+de-souza@users.noreply.github.com>
2019-10-29 17:28:48 +0100

    Add script to list reachable IPs on the network

---
+++ Unix/reachable_ips.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Print the IP adresses that are reachable on the same network
+timeout=5
+
+[[ $(ip --brief addr) =~ UP\ *([^\/]*) ]] && ip=${BASH_REMATCH[1]} || exit 1
+
+echo "This computer's IP address: $ip"
+echo "Other IPs on the network:"
+
+for i in $(seq -f "${ip%\.*}.%g" 0 254); do
+    (ping "$i" -c 1 -w "$timeout" >/dev/null && echo "$i" &)
+done
+sleep "$timeout"
