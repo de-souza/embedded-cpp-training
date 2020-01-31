b93318d4536cea2fc0dce42ef9c462a72f4ac0c0
de-souza <43355143+de-souza@users.noreply.github.com>
2020-01-31 12:18:53 +0100

    Remove parentheses

--- C/1_introduction_to_c/1.10_arrays/main.c
+++ C/1_introduction_to_c/1.10_arrays/main.c
@@ -6,7 +6,7 @@ int main()
     int size=30, arr[size], inv_arr[size];
     arr[0] = inv_arr[size-1] = 1;
     for (int i=1; i < size; i++)
-        arr[i] = inv_arr[size-(i+1)] = 2 * arr[i-1];
+        arr[i] = inv_arr[size-1-i] = 2 * arr[i-1];
     for (int i=0; i < size; i++)
         printf("arr[%d] = %d, inv_arr[%d] = %d\n", i, arr[i], i, inv_arr[i]);
 }
